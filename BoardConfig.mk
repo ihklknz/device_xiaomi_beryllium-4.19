3de04b8cd425d4c2466f5c9f3fd93e584441b8a1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-07 12:43:25 +0530

    beryllium: Enable Power HAL interaction boost
    Signed-off-by: Shikhar <Krtonia@gmail.com>
    Change-Id: I2716518b45176c5a39fb1b6214b07c01376dd79f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,7 @@ TARGET_BOARD_PLATFORM := sdm845
 
 # Power
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
