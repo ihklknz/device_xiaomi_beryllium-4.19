720b20b7d42d16201fe58a9800b6bb025ce845df
PainKiller3 <ninadpatil100@gmail.com>
2022-02-20 20:53:48 +0530

    beryllium: Remove IMS & DPM Symlinks
    * Remove sepolicy rules for older IMS blobs
    
    This reverts commit 09ec37c & a5ab783.
    
    Change-Id: I0f62585111ae4d268386dfd1b720c7dad63e9f0c

--- extract-files.sh
+++ extract-files.sh
@@ -24,6 +24,14 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/camera/components/com.qti.node.watermark.so)
+            grep -q "libpiex_shim.so" "${2}" || ${PATCHELF} --add-needed "libpiex_shim.so" "${2}"
+            ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
