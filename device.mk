5e1d4292270d1529a88ea5f839a0de9b18bddb78
johnmart19 <johnivan19999@gmail.com>
2024-01-20 12:56:59 +0530

    beryllium: Build android.hardware.media.omx
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- device.mk
+++ device.mk
@@ -229,6 +229,9 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service \
+    libstagefright_omx.vendor \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libc2dcolorconvert \
