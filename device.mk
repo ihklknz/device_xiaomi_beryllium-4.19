4cc445a9e06ede6445dc57f03d7c7677f1feb01f
ihklknz <haikalluthfianinobalukia@proton.me>
2024-01-28 14:37:31 +0000

    beryllium: Switch to libprotobuf-cpp prebuilts

--- device.mk
+++ device.mk
@@ -344,7 +344,8 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-vendorcompat \
+    libprotobuf-cpp-lite-vendorcompat
 
 # QTI
 PRODUCT_PACKAGES += \
